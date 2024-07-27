21e8fb18fbe0e3af513eb59115ef6458dcad93be
Akash <akash@breezeware.net>
2024-07-27 14:56:47 +0530

    Add security with Basic Authentication

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.example.demo.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.exception.ErrorResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/actuator/health", "/api/api-docs/**", "/api/swagger-ui/**", "/api/swagger/**")
+                .permitAll().anyRequest().authenticated()).httpBasic(withDefaults())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint()));
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            ErrorResponse errorResponse = buildErrorResponse(response, authException);
+            buildServletOutputStream(response, errorResponse);
+        };
+    }
+
+    private void buildServletOutputStream(HttpServletResponse response, ErrorResponse errorResponse)
+            throws IOException {
+        ServletOutputStream out = response.getOutputStream();
+        new ObjectMapper().writeValue(out, errorResponse);
+        out.flush();
+    }
+
+    private ErrorResponse buildErrorResponse(HttpServletResponse httpServletResponse, Exception ex) {
+        List<String> details = new ArrayList<>();
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatusCode(httpServletResponse.getStatus());
+        errorResponse.setMessage(HttpStatus.valueOf(httpServletResponse.getStatus()).name());
+        details.add(ex.getLocalizedMessage().split(":")[0]);
+        errorResponse.setDetails(details);
+        return errorResponse;
+    }
+}
